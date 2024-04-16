c193864c043c52db4333c8c360700adc2d473d86
HeeyeonKim <HeeyeonKim18@github.com>
2024-04-16 23:16:58 +0900

    first commit

---
+++ src/test/java/class101/yeoni/io/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package class101.yeoni.io.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
